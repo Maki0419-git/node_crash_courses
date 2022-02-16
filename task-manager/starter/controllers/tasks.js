d152efafd8c6c38399c97fb70a9ae865dbff2d7b
Maki0419-git <yoshinomaki90530@gmail.com>
2022-02-16 23:45:22 +0800

    day12 getsingle task

--- task-manager/starter/controllers/tasks.js
+++ task-manager/starter/controllers/tasks.js
@@ -1,16 +1,38 @@
 const Task = require('../models/tasks');
 
-const getAllTasks = (req, res) => {
-    res.send('get all tasks')
+const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({ tasks })
+    } catch (e) {
+        res.status(500).json({ msg: e });
+    }
 }
 
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body);
-    res.status(200).json({ task });
+    try {
+        const task = await Task.create(req.body);
+        res.status(200).json({ task });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ msg: e });
+    }
 }
 
-const getSingleTask = (req, res) => {
-    res.send(`getSingleTask ${req.params.id}`);
+const getSingleTask = async (req, res) => {
+    // res.send(`getSingleTask ${req.params.id}`);
+    try {
+        const task = await Task.findOne({ _id: req.params.id });
+        if (!task) {
+            //right syntax
+            return res.status(404).json({ msg: 'Task not found' });
+        }
+        res.status(200).json({ task });
+    } catch (e) {
+        console.log(e);
+        //error syntax
+        res.status(404).json({ msg: e });
+    }
 }
 
 const updateTask = (req, res) => {
--- task-manager/starter/models/tasks.js
+++ task-manager/starter/models/tasks.js
@@ -1,8 +1,35 @@
 const mongoose = require('mongoose');
 
+//ver 1.0
+// const TaskSchema = new mongoose.Schema({
+//     name: String, complete: Boolean
+// })
 
+// random will be ignore if we send it 
+// {
+// 	"name":"sogo",
+// 	"complete":true,
+//     "random":"random"
+// }
+// automatically change false to "false"，change "0" to false
+// {
+// 	"name":false,
+// 	"complete":"0"
+// }
+
+// ver 2.0
 const TaskSchema = new mongoose.Schema({
-    name: String, complete: Boolean
+    name: {
+        type: String,
+        required: [true, 'must provide a name'],
+        trim: true,
+        maxlength: [20, 'name must be at least 20 characters']
+    },
+    complete: {
+        type: Boolean,
+        default: false,
+    }
 })
+
 
 module.exports = mongoose.model('Task', TaskSchema);
