8206936af4f1458d4d43fa5fa3214b5f35970352
Maki0419-git <yoshinomaki90530@gmail.com>
2022-02-25 21:56:21 +0800

    day15 select sort

--- store-api/starter/controllers/products.js
+++ store-api/starter/controllers/products.js
@@ -7,6 +7,38 @@ const getAllProducts = async (req, res, next) => {
 
 }
 
+const getAllProductsByQuery = async (req, res) => {
+    const products = await Product.find({ price: { $gt: 100, $lt: 200 } }).select("name price").sort("price")
+    res.status(200).send({ msg: "success", products, nbHits: products.length });
+}
+
+const geAllSortedProducts = async (req, res) => {
+    const { sort } = req.query;
+    console.log(sort);
+    let result = Product.find({});
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createAt');
+    }
+    const products = await result;
+    // const products = await Product.find({}).sort('-name price');
+    res.status(200).send({ msg: "success", products, nbHits: products.length });
+}
+
+const geAllSelectedPoducts = async (req, res) => {
+    const { fields } = req.query;
+    let result = Product.find({});
+    if (fields) {
+        const fieldList = fields.split(',').join(' ');
+        result = result.select(fieldList);
+    }
+    const products = await result;
+    // const products = await Product.find({}).sort('-name price');
+    res.status(200).send({ msg: "success", products, nbHits: products.length });
+}
+
 const gatAllProductsFilterByRegex = async (req, res, next) => {
     const { search } = req.query;
     const products = await Product.find({
@@ -15,9 +47,16 @@ const gatAllProductsFilterByRegex = async (req, res, next) => {
     res.status(200).send({ msg: "success", products, nbHits: products.length });
 }
 
-const getAllProductsWithQueryFilter = async (req, res) => {
+
+const getSingleProduct = async (req, res) => {
+    const { productID } = req.body;
+    const product = await Product.findById(productID);
+    res.status(200).send({ msg: "success", product });
+}
+
+const getAllProductsFinal = async (req, res) => {
     // const products = await Product.find({ featured: true });
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
@@ -26,17 +65,47 @@ const getAllProductsWithQueryFilter = async (req, res) => {
         queryObject.company = company;
     }
     if (name) {
-        queryObject.name = name;
+        queryObject.name = { $regex: name, $options: 'i' };
     }
-    const products = await Product.find(queryObject);
+    if (numericFilters) {
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte'
+        };
+        const regEx = /\b(<|>|<=|>=|=)\b/g
+        let filters = numericFilters.replace(regEx, (match) => `--${operatorMap[match]}--`);
+        console.log(filters);
+        const options = ['price', 'rating'];
+        filters = filters.split(',').forEach(item => {
+            const [filed, operator, value] = item.split('--');
+            if (options.includes(filed)) {
+                queryObject[filed] = { [operator]: Number(value) }
+            }
+        })
+    }
+    let result = Product.find(queryObject);
+    if (sort) {
+        let sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createAt');
+    }
+    if (fields) {
+        const fieldList = fields.split(',').join(' ');
+        result = result.select(fieldList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+    result = result.limit(limit).skip(skip);
+    const products = await result;
     res.status(200).send({ msg: "success", products, nbHits: products.length });
 }
 
-const getSingleProduct = async (req, res) => {
-    const { productID } = req.body;
-    const product = await Product.findById(productID);
-    res.status(200).send({ msg: "success", product });
-}
 
 
-module.exports = { getAllProducts, getAllProductsWithQueryFilter, gatAllProductsFilterByRegex, getSingleProduct };
+module.exports = { getAllProducts, getAllProductsByQuery, geAllSelectedPoducts, getAllProductsFinal, geAllSortedProducts, gatAllProductsFilterByRegex, getSingleProduct };
--- store-api/starter/routes/products.js
+++ store-api/starter/routes/products.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getSingleProduct, getAllProducts, getAllProductsWithQueryFilter, gatAllProductsFilterByRegex } = require('../controllers/products');
+const { getSingleProduct, getAllProductsByQuery, geAllSelectedPoducts, getAllProducts, geAllSortedProducts, getAllProductsFinal, gatAllProductsFilterByRegex } = require('../controllers/products');
 
 
 router.route('/').post(getSingleProduct).get(getAllProducts);
-router.route('/filter').get(getAllProductsWithQueryFilter);
+router.route('/filter').get(getAllProductsFinal);
 router.route('/regex').get(gatAllProductsFilterByRegex);
+router.route('/sort').get(geAllSortedProducts);
+router.route('/select').get(geAllSelectedPoducts);
+router.route('/query').get(getAllProductsByQuery);
 
 
 module.exports = router;
