32a870eaa3902f470e3425f1e17179a0dce67085
Maki0419-git <yoshinomaki90530@gmail.com>
2022-03-01 21:11:59 +0800

    register day16

---
+++ job-api/app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+//router
+const mainRouter = require('./routes/main')
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
+
+//DB
+const connectDB = require('./db/connect');
+
+//middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleWare = require('./middleware/err');
+
+//middleware
+app.use(express.static('./public'));
+app.use(express.json());
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleWare);
+
+const port = process.env.PORT || 3000;
+
+const start = async (req, res) => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, console.log(`server is listening on ${port}`))
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+start();
---
+++ job-api/controllers/auth.js
@@ -0,0 +1,16 @@
+const User = require('../models/user');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError } = require('../errors');
+
+
+
+const register = async (req, res) => {
+    const user = await User.create({ ...req.body });
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token: user.createJWT() });
+}
+
+const login = async (req, res) => {
+    res.send('login user');
+}
+
+module.exports = { register, login }
