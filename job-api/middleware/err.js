14a5a2ab7de37a530476bb7ac187a8984c28b3d3
Maki0419-git <yoshinomaki90530@gmail.com>
2022-03-03 22:43:55 +0800

    err

--- job-api/middleware/err.js
+++ job-api/middleware/err.js
@@ -1,12 +1,23 @@
-const { CustomAPIError } = require('../errors');
+
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleWare = (err, req, res, next) => {
-    console.log(err.message);
-    if (err instanceof CustomAPIError) {
-        return res.status(err.statusCode).json({ msg: err.message });
+    let customError = {
+        //set default
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'Something went wrong try again later'
+
     }
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err: err.message });
+    // if (err instanceof CustomAPIError) {
+    //     return res.status(err.statusCode).json({ msg: err });
+    // }
+    if (err.code && err.code === 11000) {
+        customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please enter another value`;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+    }
+    return res.status(customError.statusCode).json(customError.msg);
+    //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+
 }
 
 module.exports = errorHandlerMiddleWare;
