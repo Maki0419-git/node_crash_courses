039711f9827251b0ac39ff8c28682453381d8f21
Maki0419-git <yoshinomaki90530@gmail.com>
2022-03-07 14:55:42 +0800

    day19

---
+++ job-api - 複製/errors/index.js
@@ -0,0 +1,5 @@
+const CustomAPIError = require('./custom-errors');
+const BadRequestError = require('./bad-request');
+const UnauthenticatedError = require('./unauthenticated');
+const NotFoundError = require('./not-found');
+module.exports = { CustomAPIError, BadRequestError, UnauthenticatedError, NotFoundError }
